5b41a367e6ba4696b34f271f00bcb27c14f14442
Jonay <jonay.rodriguez@outlook.com>
2024-01-14 14:34:37 +0000

    astro deploy page

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+const SERVER_PORT = 3000;
+const LIVE_URL = 'https://JJonnick.github.io';
+const LOCAL_URL = `http://localhost:${SERVER_PORT}`;
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCAL_URL;
+
+if (isBuild) {
+    BASE_URL = LIVE_URL;
+}
+
+// https://astro.build/config
+export default defineConfig({
+    server: { port: SERVER_PORT },
+    site: BASE_URL,
+    outDir: 'public',
+    publicDir: 'static',
+});
