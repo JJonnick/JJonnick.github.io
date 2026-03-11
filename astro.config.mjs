7db5d63003933faf0c70c0f7641bc781403396c9
Jonay <jonay.rodriguez@outlook.com>
2026-03-11 23:10:57 +0000

    fix index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,9 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-const SERVER_PORT = 4321;
-const LIVE_URL = 'https://JJonnick.github.io';
-const LOCAL_URL = `http://localhost:${SERVER_PORT}`;
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCAL_URL;
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
-
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: SERVER_PORT
-  },
-  site: BASE_URL,
+  site: 'https://jjonnick.github.io',
   fonts: [
     {
       provider: fontProviders.fontsource(),
